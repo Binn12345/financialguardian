4986f634fbc8d903dcbb5cbb593b443164685c94
Binn12345 <arvincejalbo18@gmail.com>
2024-02-04 11:06:45 +0800

    temporary push

---
+++ resources/views/components/welcome.blade.php
@@ -0,0 +1,39 @@
+
+
+<div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+    <!-- <x-application-logo class="block h-12 w-auto" /> -->
+    <h1 class="text-2xl font-medium text-gray-900">
+        Welcome {{ STRTOUPPER(Auth::user()->name); }}
+    </h1>
+
+    <p class="mt-2 text-gray-500 leading-relaxed">
+        <!-- Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
+        to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
+        you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
+        ecosystem to be a breath of fresh air. We hope you love it. -->
+        
+    </p>
+</div>
+<div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8">
+    <!-- <div> -->
+        
+        <div class="flex flex-col justify-between p-4 leading-normal bg-white  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 p-6 lg:p-8 ">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        </div>
+    <!-- <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt="">
+        
+    </a> -->
+
+    <!-- </div> -->
+    <!-- <div class="flex items-center">
+        sdlpsal[pdl]
+    </div>
+    <div class="flex items-center">
+        sdlpsal[pdl]
+    </div>
+    <div class="flex items-center">
+        sdlpsal[pdl]
+    </div> -->
+</div>
